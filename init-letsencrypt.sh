6ce17a4bcf396efcac4831b638cb57344aee834a
luo <978337784@qq.com>
2019-11-13 22:35:35 +0800

    update

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(dabaodz.cn)
+domains=($1)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
