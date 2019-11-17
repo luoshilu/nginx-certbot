7d3af6cbfd868344ff0a92630e342cda2f212dad
luo <978337784@qq.com>
2019-11-17 23:23:33 +0800

    update

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=($1)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -64,7 +64,7 @@ case "$email" in
 esac
 
 # Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--dry-run"; fi
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
